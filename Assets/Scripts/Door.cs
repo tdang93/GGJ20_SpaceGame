652538e3ebed57040eb5798cdba21862918f5ba9
EdCarrasco <carrasco.edw@gmail.com>
2020-02-02 14:43:10 -0800

    stuff

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Door : MonoBehaviour, IInteractable
 {
+    private int MAX_HEALTH = 7;
+    private int health = 7;
     private bool isOpen = false;
     private Vector3 openPosition;
     private Vector3 closedPosition;
     private SphereCollider sphereCollider;
     // Start is called before the first frame update
+    private Renderer wall_rend;
+    // Start is called before the first frame update
     void Start()
     {
+        wall_rend = GetComponent<Renderer>();
         openPosition = new Vector3(transform.position.x, transform.position.y - 1f, transform.position.z);
         closedPosition = transform.position;
     }
@@ -55,6 +60,21 @@ public class Door : MonoBehaviour, IInteractable
 
     public void repair(GameObject go) {
         Debug.Log("Panel::repair() " + go);
+        this.changeHealth(1);
+    }
+
+    private void changeHealth(int amount) {
+        this.health += amount;
+        if (this.health > this.MAX_HEALTH) this.health = this.MAX_HEALTH;
+        if (this.health < 0) this.health = 0;
+
+        if (this.health >= 5) {
+            wall_rend.material.color = Color.blue;
+        } else if (this.health >= 3 ) {
+            wall_rend.material.color = Color.gray;
+        } else {
+            wall_rend.material.color = Color.red;
+        }
     }
 
     public void rotate(GameObject go, int direction) {
