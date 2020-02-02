b32c68413eed860345434c3c8921a8c131346daf
EdCarrasco <carrasco.edw@gmail.com>
2020-02-02 10:23:13 -0800

    asteroids destroyed by gun drop resource

---
+++ Assets/Room.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// OnCollisionStay is called once per frame for every collider/rigidbody
+    /// that is touching rigidbody/collider.
+    void OnCollisionStay(Collision other)
+    {
+        // if (other.transform.tag == "Player") {
+        //     Debug.Log(this.transform.name + " is touching " + other.transform.name);
+        // }
+    }
+}
